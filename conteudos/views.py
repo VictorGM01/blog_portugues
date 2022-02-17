18789dc493022e7a3b0f448c3a22eccda88d9882
VictorGM01 <victormarques8801@gmail.com>
2022-02-17 13:48:59 -0300

    Inicia personalização dos posts na página de index, incluindo a paginação

--- conteudos/views.py
+++ conteudos/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from .models import Post
+from django.core.paginator import Paginator
 
 
 def index(request):
     principais = Post.objects.filter(principal=True)[:2]
     outros_favs = Post.objects.filter(principal=True)[2:]
-    posts = Post.objects.filter(principal=False)
+    posts = Post.objects.all().order_by('data_postagem')
+
+    paginator = Paginator(posts, 1)
+
+    pagina = request.GET.get('page')
+
+    posts_por_pagina = paginator.get_page(pagina)
+
     contexto = {
-        'posts': posts,
+        'posts': posts_por_pagina,
         'principais': principais,
         'favs': outros_favs
     }
