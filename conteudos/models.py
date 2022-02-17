39ab0bb44f3db4e6583a7310463c29f670c4aa92
VictorGM01 <victormarques8801@gmail.com>
2022-02-17 13:47:44 -0300

    Personaliza campo de conteúdo com ckeditor

--- conteudos/models.py
+++ conteudos/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from datetime import datetime
+from ckeditor.fields import RichTextField
 
 
 class Post(models.Model):
     titulo = models.CharField(max_length=100)
     legenda = models.CharField(max_length=200)
-    conteudo = models.CharField(max_length=1000)
+    conteudo = RichTextField()
     mapa_mental = models.ImageField(upload_to='imagens/%d/%m')
     data_postagem = models.DateField(default=datetime.today)
     principal = models.BooleanField(default=False)
